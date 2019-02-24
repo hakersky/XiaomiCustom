201df2b1fd4ea17170e925fa49a4c2e1975fd1a5
Kamil <hakersky1@gmail.com>
2019-02-24 19:41:27 +0000

    XiaomiCustom: remove dir

--- aptX/aptX.mk
+++
@@ -1,5 +0,0 @@
-APTX_DIR := vendor/XiaomiCustom/aptX
-
-PRODUCT_COPY_FILES += \
-    $(APTX_DIR)/lib64/libaptXHD_encoder.so:system/lib64/libaptXHD_encoder.so \
-    $(APTX_DIR)/lib64/libaptX_encoder.so:system/lib64/libaptX_encoder.so
